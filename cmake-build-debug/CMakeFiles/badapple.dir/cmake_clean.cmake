26978384d155bac8d5d4b46a3d409aadeee51f52
Hokori <yakumoy847@gmail.com>
2019-09-05 16:37:57 +0800

    add

---
+++ cmake-build-debug/CMakeFiles/badapple.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/badapple.dir/main.c.obj"
+  "badapple.pdb"
+  "badapple.exe"
+  "badapple.exe.manifest"
+  "libbadapple.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/badapple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
